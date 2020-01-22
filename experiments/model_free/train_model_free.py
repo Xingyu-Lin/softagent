73ee436443800e6c0c6d721a642d8b63dc66cd0b
Xingyu-Lin <abclin6693@gmail.com>
2020-01-22 16:59:23 -0500

    minor bug fixed

--- experiments/model_free/train_model_free.py
+++ experiments/model_free/train_model_free.py
@@ -26,9 +26,13 @@ def run_task(arg_vv, log_dir, exp_name):
 
     # Configure torch
     if torch.cuda.is_available():
-        device = torch.device('cuda:1') if torch.cuda.device_count() > 1 else torch.device('cuda:0')
+        if torch.cuda.device_count() > 1:
+            ptu.set_gpu_mode(True, 1)
+            device = torch.device('cuda:1')
+        else:
+            ptu.set_gpu_mode(True, 0)
+            device = torch.device('cuda:0')
         torch.cuda.manual_seed(vv['seed'])
-        ptu.set_gpu_mode(True, 1)
     else:
         device = torch.device('cpu')
 
