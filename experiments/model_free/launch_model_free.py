b47c5f9614f7fca8992e2b1c9155b38ff72ab2b4
yufeiwang63 <yufeiwang63@gmail.com>
2020-03-08 19:24:38 -0400

    launch_,model_free

--- experiments/model_free/launch_model_free.py
+++ experiments/model_free/launch_model_free.py
@@ -6,20 +6,33 @@ from experiments.model_free.train_model_free import run_task
 from softgym.registered_env import env_arg_dict
 
 
+
+replay_buffer_size = {
+    'key_point': int(1E6),
+    'point_cloud': int(2E5),
+    'cam_rgb': int(8E4)
+}
+
 @click.command()
 @click.argument('mode', type=str, default='local')
 @click.option('--debug/--no-debug', default=True)
 @click.option('--dry/--no-dry', default=False)  # mainly for debug
 def main(mode, debug, dry):
-    exp_prefix = '0205_sac_cloth_flatten_updated_cam_rgb'
+    exp_prefix = '2-19-water-model-free-full-state'
     vg = VariantGenerator()
-    vg.add('env_name', ['ClothFlatten'])
+    if debug:
+        vg.add('env_name', ['PourWater'])
+    else:
+        vg.add('env_name', ['PourWater', 'PassWater',]) #'ClothDrop', 'ClothFlatten', 'ClothFold', 'RopeFlatten'])
     vg.add('env_kwargs', lambda env_name: [env_arg_dict[env_name]])
-    vg.add('env_kwargs_observation_mode', ['cam_rgb'])
-    vg.add('algorithm', ['SAC'])
+    vg.add('env_kwargs_observation_mode', ['point_cloud'])
+    if not debug:
+        vg.add('algorithm', ['TD3', 'SAC'])
+    else:
+        vg.add('algorithm', ['TD3'])
     vg.add('version', ['normal'])
     vg.add('layer_size', [256])
-    vg.add('replay_buffer_size', lambda env_kwargs_observation_mode: [int(8E4) if env_kwargs_observation_mode == 'cam_rgb' else int(1E6)])
+    vg.add('replay_buffer_size', lambda env_kwargs_observation_mode: [replay_buffer_size[env_kwargs_observation_mode]])
     vg.add('embedding_size', [256])
     vg.add('image_dim', [128])
     vg.add('trainer_kwargs', lambda algorithm: [dict(discount=0.99,
@@ -30,12 +43,16 @@ def main(mode, debug, dry):
                                                      reward_scale=1,
                                                      use_automatic_entropy_tuning=True,
                                                      ) if algorithm == 'SAC' else dict(discount=0.99,
-                                                                                       policy_learning_rate=1e-3,
-                                                                                       qf_learning_rate=1e-3)])
+                                                                                       policy_learning_rate=3e-4,
+                                                                                       qf_learning_rate=3e-4, )])
     vg.add('max_episode_length', [200])
-    vg.add('seed', [100, 200, 300, 400, 500])
+    if debug:
+        vg.add('seed', [100])
+    else:
+        vg.add('seed', [100, 200])
+
     if not debug:
-        vg.add('algorithm_kwargs', [dict(num_epochs=500,
+        vg.add('algorithm_kwargs', [dict(num_epochs=2000,
                                          num_eval_steps_per_epoch=900,
                                          num_trains_per_train_loop=1000,
                                          num_expl_steps_per_train_loop=1000,
@@ -45,10 +62,10 @@ def main(mode, debug, dry):
                                          dump_policy_video_interval=20)])
     else:
         vg.add('algorithm_kwargs', [dict(num_epochs=500,
-                                         num_eval_steps_per_epoch=120,
-                                         num_trains_per_train_loop=2,
-                                         num_expl_steps_per_train_loop=120,
-                                         min_num_steps_before_training=120,
+                                         num_eval_steps_per_epoch=900,
+                                         num_trains_per_train_loop=1,
+                                         num_expl_steps_per_train_loop=1000,
+                                         min_num_steps_before_training=1000,
                                          max_path_length=200,
                                          batch_size=256,
                                          dump_policy_video_interval=1)])
@@ -84,8 +101,8 @@ def main(mode, debug, dry):
         )
         if cur_popen is not None:
             sub_process_popens.append(cur_popen)
-        if debug:
-            break
+        # if debug:
+        #     break
 
 
 if __name__ == '__main__':
