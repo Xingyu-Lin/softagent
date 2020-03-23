6391d797fd9bfa47489480456d9897976a1329fa
Xingyu-Lin <abclin6693@gmail.com>
2020-03-23 16:26:43 -0400

    resrl in progress

---
+++ ResRL/train.py
@@ -0,0 +1,75 @@
+from cem.cem import CEMPolicy
+from experiments.planet.train import update_env_kwargs
+from experiments.cem.record_cem import cem_make_gif
+from planet.utils import transform_info
+from envs.env import Env
+from chester import logger
+import torch
+import pickle
+import os
+import os.path as osp
+import copy
+import multiprocessing as mp
+import json
+import numpy as np
+
+
+def run_task(arg_vv, log_dir, exp_name):
+    mp.set_start_method('spawn')
+    vv = arg_vv
+    vv = update_env_kwargs(vv)
+
+    # Configure logger
+    logger.configure(dir=log_dir, exp_name=exp_name)
+    logdir = logger.get_dir()
+    assert logdir is not None
+    os.makedirs(logdir, exist_ok=True)
+
+    # Configure torch
+    if torch.cuda.is_available():
+        if torch.cuda.device_count() > 1:
+            device = torch.device('cuda:1')
+        else:
+            device = torch.device('cuda:0')
+        torch.cuda.manual_seed(vv['seed'])
+
+    # Dump parameters
+    with open(osp.join(logger.get_dir(), 'variant.json'), 'w') as f:
+        json.dump(vv, f, indent=2, sort_keys=True)
+    env_symbolic = vv['env_kwargs']['observation_mode'] != 'cam_rgb'
+    env_class = Env
+    env_kwargs = {'env': vv['env_name'],
+                  'symbolic': env_symbolic,
+                  'seed': vv['seed'],
+                  'max_episode_length': vv['max_episode_length'],
+                  'action_repeat': 1,
+                  'bit_depth': 8,
+                  'image_dim': None,
+                  'env_kwargs': vv['env_kwargs']}
+    env = env_class(**env_kwargs)
+
+    # Run policy
+    initial_states, action_trajs, configs, all_infos = [], [], [], []
+    for i in range(vv['test_episodes']):
+        logger.log('episode ' + str(i))
+        obs = env.reset()
+        initial_state = env.get_state()
+        action_traj = []
+        infos = []
+        for _ in range(env.horizon):
+            action = policy.get_action(obs)
+            action_traj.append(copy.copy(action))
+            obs, reward, _, info = env.step(action)
+            infos.append(info)
+        all_infos.append(infos)
+        initial_states.append(initial_state.copy())
+        action_trajs.append(action_traj.copy())
+        configs.append(env.get_current_config().copy())
+
+        # Log for each episode
+        transformed_info = transform_info([infos])
+        for info_name in transformed_info:
+            logger.record_tabular('info_' + 'final_' + info_name, transformed_info[info_name][0, -1])
+            logger.record_tabular('info_' + 'avarage_' + info_name, np.mean(transformed_info[info_name][0, :]))
+            logger.record_tabular('info_' + 'sum_' + info_name, np.sum(transformed_info[info_name][0, :], axis=-1))
+        logger.dump_tabular()
