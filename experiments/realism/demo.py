fde8f2277a57214ef105b42b0c53530f919a74b4
Xingyu-Lin <abclin6693@gmail.com>
2020-04-17 00:13:33 -0400

    finsih demo

--- experiments/realism/demo.py
+++ experiments/realism/demo.py
@@ -6,11 +6,12 @@ from softgym.utils.normalized_env import normalize
 
 
 def generate_pick_and_place(sx, sy, ex, ey, zl, zh):
+    a0 = [sx, zh, sy, 0]
     a1 = [sx, zl, sy, 0]
     a2 = [sx, zh, sy, 1]
     a3 = [ex, zh, ey, 1]
     a4 = [ex, zh, ey, 0]
-    actions = np.array([a1, a2, a3, a4])
+    actions = np.array([a0, a1, a2, a3, a4])
     return actions
 
 
@@ -18,14 +19,23 @@ def get_robot_data():
     pickpts, placepts = np.load('experiments/realism/pickpts.npy'), np.load('experiments/realism/placepts.npy')
     centerx = 0.525
     centery = -0.02
+
     pickpts[:, 0] -= centerx
     placepts[:, 0] -= centerx
     pickpts[:, 1] -= centery
     placepts[:, 1] -= centery
-    return pickpts, placepts
+    sim_pickpts = np.zeros_like(pickpts)
+    sim_placepts = np.zeros_like(placepts)
+    sim_pickpts[:, 0] = pickpts[:, 1]
+    sim_placepts[:, 0] = placepts[:, 1]
+    sim_pickpts[:, 1] = pickpts[:, 0]
+    sim_placepts[:, 1] = placepts[:, 0]
+    # Transform the coordinates
+    return sim_pickpts, sim_placepts
 
 
 if __name__ == '__main__':
+    particle_radius = 0.00625
     generate_cache = False
     if generate_cache:
         env = ClothFoldEnv(
@@ -41,13 +51,14 @@ if __name__ == '__main__':
             use_cached_states=False,
             save_cache_states=False,
             deterministic=True)
-        config = {'ClothPos': [-0.35, -0.17, -0.184], 'ClothSize': [115, 60], 'ClothStiff': [0.8, 0.25, 0.25], 'camera_name': 'default_camera',
+        config = {'ClothPos': [-0.31, -0.2, -0.184], 'ClothSize': [int(0.6 / particle_radius), int(0.368 / particle_radius)],
+                  'ClothStiff': [0.8, 1, 0.9], 'camera_name': 'default_camera',
                   'camera_params': {'default_camera': {'pos': np.array([0.0, 1.4, 0.6]),
                                                        'angle': np.array([0, -60 / 180. * np.pi, 0.]),
                                                        'width': 720,
-                                                       'height': 720}}, 'env_idx': 14,
-                  'BlockSize': [0.05, 0.05, 0.05], 'BlockPos': [-0.35, 0.2, 0.2]}
+                                                       'height': 720}}, 'env_idx': 14, 'mass': 0.3}
         env.cached_configs, env.cached_init_states = env.generate_env_variation(1, vary_cloth_size=False, config=config, save_to_file=True)
+        exit()
     else:
         env = ClothFoldEnv(
             observation_mode='cam_rgb',
@@ -63,7 +74,7 @@ if __name__ == '__main__':
             save_cache_states=False,
             deterministic=True,
             picker_radius=0.03,
-            particle_radius=0.00625)
+            particle_radius=particle_radius)
     pickpts, placepts = get_robot_data()
 
     # env = normalize(env) TODO: No normalization as actions matter here. Be cautious when actually using the pick and place action space
@@ -72,11 +83,10 @@ if __name__ == '__main__':
     for i in range(pickpts.shape[0]):
         env.reset()
         env.action_tool.update_picker_boundary([-np.inf] * 3, [np.inf] * 3)
-        if i ==0:
-            actions = generate_pick_and_place(-0.25, 0.15, 0.2, -0.1, 0.2, 0.4)
-        else:
-            actions = generate_pick_and_place(-pickpts[i, 0], -pickpts[i, 1], -placepts[i, 0], -placepts[i, 1], 0.2, 0.2 + 0.1065
-                                              )
+        # actions = generate_pick_and_place(0, 0, 0, 0, 0.2, 0.2 + 0.1065
+
+        actions = generate_pick_and_place(pickpts[i, 0], pickpts[i, 1] - 0.06, placepts[i, 0] , placepts[i, 1]-0.06, 0.2, 0.2 + 0.1065)
+
         for action in actions:
             print('action:', action)
             env.step(action)
@@ -84,6 +94,4 @@ if __name__ == '__main__':
             pyflex.step()
             if env.recording:
                 env.video_frames.append(env.render(mode='rgb_array'))
-        if i == 2:
-            break
     env.end_record(video_path='./experiments/realism/demo.gif')
