4bb1a4dc06531bdd0dfac144252b7931dd45e6c1
Xingyu-Lin <abclin6693@gmail.com>
2020-07-29 23:46:58 -0400

    minor

--- experiments/generate_cached_states.py
+++ experiments/generate_cached_states.py
@@ -41,7 +41,7 @@ def generate_env_state(env_name):
     kwargs = env_arg_dict[env_name]
     kwargs['headless'] = True
     kwargs['use_cached_states'] = False
-    kwargs['num_variations'] = 1000
+    kwargs['num_variations'] = 10
     kwargs['save_cached_states'] = True
 
     # Env wrappter
@@ -51,7 +51,7 @@ def generate_env_state(env_name):
 
 if __name__ == '__main__':
     env_names = ['ClothFold', 'ClothFlatten', 'ClothDrop', 'ClothFoldCrumpled', 'ClothFoldDrop']
-    env_names = ['RigidClothFold']
+    env_names = ['ClothFold']
 
     for env_name in env_names:
         # p = Process(target=generate_env_state, args=(env_name,))
