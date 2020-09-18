457341a3b2fb9a3c7f541da6772b53c2a41a201a
yufeiwang63 <yufeiwang63@gmail.com>
2020-09-18 10:10:00 -0400

    minor change

--- tests/test_sawyer.py
+++ tests/test_sawyer.py
@@ -1,46 +1,64 @@
 import numpy as np
 import pyflex
 from softgym.envs.cloth_fold import ClothFoldEnv
-from softgym.envs.pass_water import PassWater1DEnv
+# from softgym.envs.pass_water import PassWater1DEnv
 # from softgym.envs.rope_flatten import RopeFlattenEnv
 import cv2
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+from softgym.registered_env import env_arg_dict, SOFTGYM_ENVS
+from softgym.utils.normalized_env import normalize
+
 
 def render_sawyer_cloth():
-    env = ClothFoldEnv(
-        observation_mode='cam_rgb',
-        action_mode='sawyer',
-        num_picker=2,
-        render=False,
-        headless=True,
-        horizon=75,
-        action_repeat=8,
-        render_mode='cloth',
-        cached_states_path='cloth_fold_test.pkl',
-        use_cached_states=True,
-        save_cached_states=False,
-        deterministic=True)
-    particle_radius = 0.00625
-    config = {'ClothPos': [-0.31, -0.6, -0.184], 'ClothSize': [int(0.6 / particle_radius), int(0.368 / particle_radius)],
-              'ClothStiff': [0.8, 1, 0.9], 'camera_name': 'default_camera',
-              'camera_params': {'default_camera': {'pos': np.array([0.0, 2.5, 1.2]),
-                                                   'angle': np.array([0, -45 / 180. * np.pi, 0.]),
-                                                   'width': 720,
-                                                   'height': 720}}, 'env_idx': 14, 'mass': 0.3}
+    # env = ClothFoldEnv(
+    #     observation_mode='cam_rgb',
+    #     action_mode='sawyer',
+    #     num_picker=2,
+    #     render=False,
+    #     headless=True,
+    #     horizon=75,
+    #     action_repeat=8,
+    #     render_mode='cloth',
+    #     cached_states_path='cloth_fold_test.pkl',
+    #     use_cached_states=True,
+    #     save_cached_states=False,
+    #     deterministic=True)
+    # particle_radius = 0.00625
+    # config = {'ClothPos': [-0.31, -0.6, -0.184], 'ClothSize': [int(0.6 / particle_radius), int(0.368 / particle_radius)],
+    #           'ClothStiff': [0.8, 1, 0.9], 'camera_name': 'default_camera',
+    #           'camera_params': {'default_camera': {'pos': np.array([0.0, 2.5, 1.2]),
+    #                                                'angle': np.array([0, -45 / 180. * np.pi, 0.]),
+    #                                                'width': 720,
+    #                                                'height': 720}}, 'env_idx': 14, 'mass': 0.3}
 
-    camera_params = config['camera_params'][config['camera_name']]
-    scene_params = np.array([*config['ClothPos'], *config['ClothSize'], *config['ClothStiff'], 2,
-                             *camera_params['pos'][:], *camera_params['angle'][:], camera_params['width'], camera_params['height'], 0.5])
-    print("before set scene")
-    pyflex.set_scene(14, scene_params, 0, [0.925])
-    exit()
+    # camera_params = config['camera_params'][config['camera_name']]
+    # scene_params = np.array([*config['ClothPos'], *config['ClothSize'], *config['ClothStiff'], 2,
+    #                          *camera_params['pos'][:], *camera_params['angle'][:], camera_params['width'], camera_params['height'], 0.5])
+    # print("before set scene")
+    # pyflex.set_scene(14, scene_params, 0, [0.925])
+    # exit()
     # states = np.load('test.npy')
     # pyflex.set_robot_state(states)
+
+    env_kwargs = env_arg_dict['ClothFold']
+
+    env_kwargs['use_cached_states'] = False
+    env_kwargs['save_cached_states'] = False
+    env_kwargs['num_variations'] = 1
+    env_kwargs['render'] = True
+    env_kwargs['headless'] = False
+    # env_kwargs['action_mode'] = 'sawyer'
+
+    env = normalize(SOFTGYM_ENVS['ClothFold'](**env_kwargs))
+    env.reset()
+
+    pyflex.set_sensor_segment(True)
     for i in range(1000):
-        pyflex.set_sensor_segment(i % 2 == 0)
-        pyflex.step(np.ones(8)*0.01, render=False)
+        action = env.action_space.sample()
+        # pyflex.step(np.ones(8)*0.01, render=False)
+        _, _, _, info = env.step(action)
         rgbd = pyflex.render_sensor()
-        states = pyflex.get_robot_state()
+        # states = pyflex.get_robot_state()
         # print(np.mean(states))
 
         # exit()
@@ -48,7 +66,12 @@ def render_sawyer_cloth():
         fig, (ax1, ax2) = plt.subplots(1, 2)
         ax1.imshow(rgbd[::-1, :, :3])
         ax2.imshow(rgbd[::-1, :, 3])
-        plt.show()
+        cv2.imshow('rgb', rgbd[::-1, :, :3])
+        cv2.imshow('depth', rgbd[::-1, :, 3])
+        cv2.waitKey()
+        print("before show")
+        # plt.show()
+        print("after show")
 
     # np.save('test.npy', states)
     print("after set scene")
